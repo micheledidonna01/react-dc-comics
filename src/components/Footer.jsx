2d27c960fab12f38a6c3953acbcd341da00b2ca2
micheledidonna01 <micheledidonna337@gmail.com>
2025-04-04 16:16:53 +0200

    feat: footer bonus in preparation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,77 +1,47 @@
-import ImageDigital from "../assets/img/buy-comics-digital-comics.png"
-import ImageMerch from "../assets/img/buy-comics-merchandise.png"
-import ImageSub from "../assets/img/buy-comics-subscriptions.png"
-import ImageShop from "../assets/img/buy-comics-shop-locator.png"
-import ImagePower from "../assets/img/buy-dc-power-visa.svg"
+
 import ImgFace from "../assets/img/footer-facebook.png"
 import ImgTwit from "../assets/img/footer-twitter.png"
 import ImgYou from "../assets/img/footer-youtube.png"
 import ImgPinterest from "../assets/img/footer-pinterest.png"
 import ImgPosit from "../assets/img/footer-periscope.png"
 
-const Footer = () => 
-<footer>
+import InfoFooter from "./InfoFooter"
+import DivMainFooter from "./DivMainFooter"
+
+const Footer = () => {
+
+let arrayMainFooter=[
+    {
+        title:'DC COMICS',
+        element: ['Characters', 'Comics', 'Movies', 'TV', 'Games', 'Videos', 'News']
+    },
+    {
+        title:'SHOP',
+        element: ['Shop DC', 'Shop DC Collectibles']
+    },
+    {
+        title:'DC',
+        element: ['Terms Of Use', 'Privacy policy(New)', 'Ad Choices', 'Adverting', 'Jobs', 'Subscriptions', 'Talent Workshops', 'CPSC Certificates', 'Ratings', 'Shop Help', 'Contact Us']
+    },
+    {
+        title:'SITES',
+        element: ['DC', 'MAD', 'DC Kids', 'DC Universe', 'DC Power Visa']
+    }
+];
+
+return <footer>
 
     <div className="dad-info-footer">
-        <div className="info-footer">
-            <ul>
-                <li><img src={ImageDigital} alt="digital comics" /> <p>DIGITAL COMICS</p> </li>
-                <li><img src={ImageMerch} alt="dc merchandise" /> <p>DC MERCHANDISE</p></li>
-                <li><img src={ImageSub} alt="subscription" /> <p>SUBSCRIPTION</p></li>
-                <li><img src={ImageShop} alt="comic shop locator" /> <p>COMIC SHOP LOCATOR</p></li>
-                <li><img src={ImagePower} alt="dc power visa" /> <p>DC POWER VISA</p></li>
-            </ul>
-        </div>
+        <InfoFooter />
     </div>
 
     <div className="dad-main-footer">
         <div className="main-footer">
             <nav>
-                <div>
-                    <h3>DC COMICS</h3>
-                    <ul>
-                        <li><a href="#">Characters</a></li>
-                        <li><a href="#">Comics</a></li>
-                        <li><a href="#">Movies</a></li>
-                        <li><a href="#">TV</a></li>
-                        <li><a href="#">Games</a></li>
-                        <li><a href="#">Videos</a></li>
-                        <li><a href="#">News</a></li>
-                    </ul>
-                </div>
-                <div>
-                    <h3>SHOP</h3>
-                    <ul>
-                        <li><a href="#">Shop DC</a></li>
-                        <li><a href="#">Shop DC Collectibles</a></li>
-                    </ul>
-                </div>
-                <div>
-                    <h3>DC</h3>
-                    <ul>
-                        <li><a href="#">Terms Of Use</a></li>
-                        <li><a href="#">Privacy policy(New)</a></li>
-                        <li><a href="#">Ad Choices</a></li>
-                        <li><a href="#">Adverting</a></li>
-                        <li><a href="#">Jobs</a></li>
-                        <li><a href="#">Subscriptions</a></li>
-                        <li><a href="#">Talent Workshops</a></li>
-                        <li><a href="#">CPSC Certificates</a></li>
-                        <li><a href="#">Ratings</a></li>
-                        <li><a href="#">Shop Help</a></li>
-                        <li><a href="#">Contact Us</a></li>
-                    </ul>
-                </div>
-                <div>
-                    <h3>SITES</h3>
-                    <ul>
-                        <li><a href="#">DC</a></li>
-                        <li><a href="#">MAD Magazine</a></li>
-                        <li><a href="#">DC Kids</a></li>
-                        <li><a href="#">DC Universe</a></li>
-                        <li><a href="#">DC Power Visa</a></li>
-                    </ul>
-                </div>
+                <DivMainFooter title = {arrayMainFooter[0].title} element = {arrayMainFooter[0].element}/>
+                <DivMainFooter title = {arrayMainFooter[1].title} element = {arrayMainFooter[1].element}/>
+                <DivMainFooter title = {arrayMainFooter[2].title} element = {arrayMainFooter[2].element}/>
+                <DivMainFooter title = {arrayMainFooter[3].title} element = {arrayMainFooter[3].element}/>
             </nav>
 
             <div className="bg-image">    </div>
@@ -97,5 +67,7 @@ const Footer = () =>
         </div>
     </div>
 </footer>
+}
+
 
 export default Footer;
